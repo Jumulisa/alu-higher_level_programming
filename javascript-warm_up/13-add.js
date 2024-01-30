20f976ec0e5045f6ce418cf5dfcf3b16337dd167
jumulisa <j.umulisa@alustudent.com>
2024-01-30 11:49:47 +0000

    script

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-const firstInt = parseInt(process.argv[2]);
-const secondInt = parseInt(process.argv[3]);
-console.log(add(firstInt, secondInt));
+exports.add = function (a, b) {
+  return a + b;
+};

2896f723cf0f5ab55b4ffaa9b9f607d5df5dca01
jumulisa <j.umulisa@alustudent.com>
2024-01-31 14:22:54 +0000

    scope

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
-const { list } = require('./100.data');
-const newList = list.map((value, index) => value * index);
-console.log(list);
+const data = require('./100-data');
+
+const initialList = data.list;
+
+// Using map to create a new list where each value is equal to the value of the initial list, multiplied by its index
+const newList = initialList.map((value, index) => value * index);
+
+// Printing both the initial list and the new list
+console.log(initialList);
 console.log(newList);
+

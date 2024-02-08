86c44b79d4cb9765a182de3f5f1c9d48954a10e4
jumulisa <j.umulisa@alustudent.com>
2024-02-08 14:32:21 +0000

    script

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
@@ -12,19 +11,5 @@ fs.readFile(filePath, 'utf-8', (err, data) => {
   } else {
     console.log(data);
   }
-=======
-t fs = require('fs');
-const filePath = process.argv[2];
-if (!filePath) {
-	  console.error('Please provide a file path as an argument');
-	  process.exit(1);
-}
-fs.readFile(filePath, 'utf-8', (err, data) => {
-	  if (err) {
-		      console.error(err);
-		    } else {
-			        console.log(data);
-			      }
->>>>>>> 2e0b19be210e0778dca7e9ac9cf03c605ce8f27d
 }
 );
